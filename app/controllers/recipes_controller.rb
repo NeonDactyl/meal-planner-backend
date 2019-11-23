9a921e085829071b421e86cb2d99024d92517fc4
Trey Westrich <treyw05@gmail.com>
2019-11-23 13:19:57 -0600

    single and all recipe json set

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,44 @@
 class RecipesController < ApplicationController
+
+    def index
+        recipes = Recipe.all
+        render json: recipes.to_json(:include => { 
+            :recipe_ingredients => {
+                :include => {
+                    :ingredient => {
+                        :only => [:name, :id]
+                    },
+                    :unit_of_measure => {
+                        :only => [:name, :id]
+                    }
+                },
+                :only => [:quantity, :id]
+            }
+        },
+        :only => [:name, :id, :instructionSet])
+    end
+
+    def create
+        
+    end
+    
+    def show
+        recipe = Recipe.find(params[:id])
+        render json: recipe.to_json(:include => { 
+            :recipe_ingredients => {
+                :include => {
+                    :ingredient => {
+                        :only => [:name, :id]
+                    },
+                    :unit_of_measure => {
+                        :only => [:name, :id]
+                    }
+                },
+                :only => [:quantity, :id]
+            }
+        },
+        :only => [:name, :id, :instructionSet])
+    end
+    
+    
 end
