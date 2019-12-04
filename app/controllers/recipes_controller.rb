ab101b550a423c54bf9ac2a06d0788b26ce64eac
NeonDactyl <treyw05@gmail.com>
2019-12-04 16:36:47 -0600

    tweaked all recipes json

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -12,7 +12,7 @@ class RecipesController < ApplicationController
                         :only => [:name, :id]
                     }
                 },
-                :only => [:quantity, :id]
+                :only => [:quantity, :id, :recipe_id]
             }
         },
         :only => [:name, :id, :instruction_set])
